5cd58fc2ad01e77f51a7a44e6fe40cbdf8c9ca27
Oleg Yun <oyun@unifonic.com>
2023-07-12 11:02:12 +0000

    analytics.lm_v_master_account_mapping.sql edited online with Bitbucket

--- analytics.lm_v_master_account_mapping.sql
+++ analytics.lm_v_master_account_mapping.sql
@@ -57,6 +57,7 @@ SELECT 			a1.account_nk AS UC_Account_ID_L0,
 					   when Charging_ID_L0 in ('10000326') then '4154'
 					   when Charging_ID_L0 in ('20000477', '10006034') then '3525'
 					   when Charging_ID_L0 in ('10000297') then '3374'
+					   when Charging_ID_L0 in ('20003684', '20005029') then '10857'
 					   else null 
 				  end as manual_odoo_id
 				, case when Charging_ID_L0 in ('20005065', '20004825', '20004828', '10004615') then 'Careem Networks FZ LLC'
@@ -66,6 +67,7 @@ SELECT 			a1.account_nk AS UC_Account_ID_L0,
 					   when Charging_ID_L0 in ('10000326') then 'Takamol Holding'
 					   when Charging_ID_L0 in ('20000477', '10006034') then 'Al-Romansiah Resturant Co .Ltd'
 					   when Charging_ID_L0 in ('10000297') then 'Rawabi Majd International Company for Production and Distribution Ltd. - Al-Majd TV'
+					   when Charging_ID_L0 in ('20003684', '20005029') then 'Food Basics Trading Company - Hamburgini'
 					   else null 
 				  end as manual_odoo_name
  from  			base
